fdd4c1bac730cf5f4c18c3c807a7b88fd72da861
Robin <robin.lerouge@etu.univ-amu.fr>
2023-03-12 20:04:46 +0100

    continuation

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<header>
+    <title>PHP Practice</title>
+</header>
+
+<body>
+    <h1>PHP Practice</h1>
+    <p>Here is some PHP code:</p>
+    <p>
+
+
+    <form method="get">
+        titre : <input type="text" name="titre" />
+        description : <input type="text" name="description" />
+        <input type="submit" name="submit" />
+    </form>
+
+
+    <?php
+    include "php/phpfunction.php";
+    
+    if (isset($_GET['submit'])) {
+        $info = formrecup();
+        $title = $info[0];
+        $description = $info[1];
+        $url = "html/" . $title . ".html";
+        if ($title == "clear") {
+            clearfile();
+            deletallfilesinhtml();
+        }
+        else{
+            file_put_contents($url,$description);
+            putintodatafile("<br> <a href=$url> $title </a>");
+        }
+    }
+
+
+    $data = readdatafile();
+    echo $data;
+    ?>
+    </p>
+
+</body>
+
+</html>
